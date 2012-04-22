ae60f5ec08da368edf24a2a38d5f5960de824975
Jasper Palfree <well.caffeinated@gmail.com>
2012-04-21 20:51:21 -0400

    Initial commit

---
+++ src/body.js
@@ -0,0 +1,22 @@
+define(
+	function(){
+
+		function Body(){
+
+			
+		}
+
+		Body.isBody = function( b ){
+
+			return (b instanceof Body);
+
+		};
+
+		Body.prototype = {
+
+			
+		};
+
+		return Body;
+	}
+);
